62a3e405d051af159c8b6c7966449ec7956e1333
Azryel <AzryelShroud@gmail.com>
2021-10-22 20:56:14 +0200

    reorder and add f1-list

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 'use strict';
 
 // Declare app level module which depends on views, and core components
+/*
 angular.module('myApp', [
   'ngRoute',
+  'myApp.f1-list',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
@@ -10,5 +12,6 @@ angular.module('myApp', [
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/f1-list'});
 }]);
+*/
---
+++ f1-list/f1-list.component.js
@@ -0,0 +1,10 @@
+"use strict";
+
+angular.
+    module("f1List").
+    component("f1List", {
+        templateUrl: "f1-list/f1-list.template.html",
+        controller: function F1ListController() {
+            console.log("F1 list log")
+        }
+    });
