e62634af6e4117d0d92c7497c2e77b8b8db04398
Azryel <AzryelShroud@gmail.com>
2021-10-23 18:13:42 +0200

    fetch data and display in tbale

--- f1-list/f1-list.component.js
+++ f1-list/f1-list.component.js
@@ -4,7 +4,14 @@ angular.
     module("f1List").
     component("f1List", {
         templateUrl: "f1-list/f1-list.template.html",
-        controller: function F1ListController() {
+        controller: [ "$http", function F1ListController($http) {
             console.log("F1 list log")
-        }
+            let self = this;
+
+            $http.get("https://ergast.com/api/f1/2021/driverStandings.json")
+                .then(response => {
+                    self.driverStandings = response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+                    console.log("DATA",response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings)
+                });
+        }]
     });
