76f73a34c5616c2b199d363d1ee50ee22a4a1f68
Azryel <AzryelShroud@gmail.com>
2021-10-25 18:17:10 +0200

    add race schedule

--- app.config.js
+++ app.config.js
@@ -5,11 +5,20 @@ angular.
   config(['$routeProvider',
     function config($routeProvider) {
       $routeProvider.
+        when('/list/driver-standings/', {
+          redirectTo:'/list/driver-standings/' + new Date().getFullYear()
+        }).
         when('/list/driver-standings/:year', {
           template: '<f1-list-year></f1-list-year>'
         }).
         when('/list/results/:year/:driverId', {
           template: '<f1-single-driver-results></f1-single-driver-results>'
+        }).
+        when('/list/schedule/', {
+          redirectTo:'/list/schedule/' + new Date().getFullYear()
+        }).
+        when('/list/schedule/:year/', {
+          template: '<f1-race-schedule></f1-race-schedule>'
         }).
         otherwise('/list/driver-standings/' + new Date().getFullYear() );
     }
